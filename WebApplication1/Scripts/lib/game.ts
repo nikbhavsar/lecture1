3afc7ec5bd5a52e6a27f514bac0ac3527677c223
nikbhavsar <bhavsarnikhar21@gmail.com>
2015-01-12 13:51:24 -0500

    basic site

---
+++ WebApplication1/Scripts/lib/game.ts
@@ -0,0 +1,13 @@
+﻿class Player {
+    strength: number;
+    constructor() {
+        this.strength = 10;
+    }
+    flights() {
+        console.log("He Flights");
+    }
+}
+function main() {
+    var tom = new Player();
+    tom.flights();
+}
